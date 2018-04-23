392f6acd4928d8106666e7b216fb41cbbb297f9c
Jianan Wang <ms17jw@msc23.tcs.cs.ox.ac.uk>
2018-04-23 17:29:46 +0100

    implementing fc_net

--- cs231n/classifiers/fc_net.py
+++ cs231n/classifiers/fc_net.py
@@ -47,6 +47,9 @@ class TwoLayerNet(object):
         # and biases using the keys 'W1' and 'b1' and second layer                 #
         # weights and biases using the keys 'W2' and 'b2'.                         #
         ############################################################################
+        self.params['W1'] = np.random.normal(scale = weight_scale) 
+        self.params['W2'] = np.random.normal(scale = weight_scale)
+        
         pass
         ############################################################################
         #                             END OF YOUR CODE                             #
